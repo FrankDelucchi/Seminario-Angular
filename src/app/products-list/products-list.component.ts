ed8d11b067cd07287304bc8b64387c564dfd4538
Franco Delucchi <fdeluccho@gmail.com>
2022-11-27 23:52:10 -0300

    agregada interfaz product y mejoras visuales

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { product } from './product';
 
 @Component({
   selector: 'app-products-list',
@@ -7,12 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  product = {
-    "name": "Zapas Nike",
-    "descprition": "The best snickers in the world",
-    "price": 80000,
-    "image": "assets/images/Nike-Mamba.jpg"
-  }
+  products : product [] = [
+    {
+      name: "Zapas Nike Kobe",
+      description: "The best snickers in the world",
+      price: 80000,
+      image: "assets/images/Nike-Mamba.jpg"
+    },
+    {
+      name: "Zapas Lillard",
+      description: "The best snickers in the world",
+      price: 80000,
+      image: "assets/images/adidas-dame-8.jpg"
+    },
+    {
+      name: "Zapas Nike Irving",
+      description: "The best snickers in the world",
+      price: 80000,
+      image: "assets/images/kyrie7.jpg"
+    },
+    {
+      name: "Zapas Nike Lebron",
+      description: "The best snickers in the world",
+      price: 80000,
+      image: "assets/images/lebron-witness-5.jpg"
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
